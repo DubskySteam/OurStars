c87d601d0186a22d95ed04c37ef57c4aed4aba98
Clemens <dubcmsky@protonmail.com>
2022-08-08 17:34:27 +0200

    update home screen

--- Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/HomeScreen.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/HomeScreen.kt
@@ -1,15 +1,36 @@
 package dev.dubsky.ourstars.android.screens.open
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import dev.dubsky.ourstars.android.composables.buttons.FullWidth
+import java.time.Month
 
 @Composable
 fun HomeScreen() {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+        ) {
+            FullWidth("Donate", 13.sp)
+            FullWidth("Login", 13.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-
+    HomeScreen()
 }
