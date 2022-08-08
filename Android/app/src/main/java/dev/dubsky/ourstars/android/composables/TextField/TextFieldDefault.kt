2158ba54808f5aadbb60ebfaa74f21c308d2e964
Clemens <dubcmsky@protonmail.com>
2022-08-08 19:44:47 +0200

    update default text-field

--- Android/app/src/main/java/dev/dubsky/ourstars/android/composables/TextField/TextFieldDefault.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/composables/TextField/TextFieldDefault.kt
@@ -1,16 +1,29 @@
 package dev.dubsky.ourstars.android.composables.TextField
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.dubsky.ourstars.android.ui.theme.Primary25
+import dev.dubsky.ourstars.android.ui.theme.White98
+import java.lang.reflect.Modifier
 
 @Composable
 fun TextFieldDefault(state: MutableState<TextFieldValue>) {
-    TextField(value = state.value, onValueChange = { state.value = it })
+    TextField(
+        value = state.value,
+        onValueChange = { state.value = it },
+        singleLine = true,
+        shape = RoundedCornerShape(13.dp),
+        colors = TextFieldDefaults.textFieldColors(backgroundColor = White98, cursorColor = Primary25, textColor = Primary25)
+    )
 }
 
 @Preview(showBackground = true)
