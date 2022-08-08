35a9d49aa61426f41cc0fddf957efbb482e10e3c
Clemens <dubcmsky@protonmail.com>
2022-08-08 16:05:07 +0200

    added basic full-width button

--- Android/app/src/main/java/dev/dubsky/ourstars/android/composables/buttons/FullWidth.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/composables/buttons/FullWidth.kt
@@ -1,15 +1,41 @@
 package dev.dubsky.ourstars.android.composables.buttons
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.dubsky.ourstars.android.ui.theme.Primary25
+import dev.dubsky.ourstars.android.ui.theme.White98
 
 @Composable
-fun FullWidth() {
-
+fun FullWidth(text: String, fontSize: TextUnit) {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = White98, contentColor = Primary25),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(13.dp))
+            .height(48.dp)
+    ) {
+        Text(text, fontSize = fontSize)
+        Icon(Icons.Filled.PlayArrow, contentDescription = null)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun FullWidthPreview() {
-
+    FullWidth("Donation", 12.sp)
 }
--- Android/app/src/main/java/dev/dubsky/ourstars/android/ui/theme/Color.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package dev.dubsky.ourstars.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White98 = Color(0xFFFAF9FA)
+val Primary25 = Color(0xFF31156B)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
