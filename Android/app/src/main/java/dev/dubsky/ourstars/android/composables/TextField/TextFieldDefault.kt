0cc8ef180be35ea710fefd62e7500578f603b731
Clemens <dubcmsky@protonmail.com>
2022-08-08 23:08:30 +0200

    updated text field to take in an icon as parameter

--- Android/app/src/main/java/dev/dubsky/ourstars/android/composables/TextField/TextFieldDefault.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/composables/TextField/TextFieldDefault.kt
@@ -1,13 +1,17 @@
 package dev.dubsky.ourstars.android.composables.TextField
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,12 +20,13 @@ import dev.dubsky.ourstars.android.ui.theme.White98
 import java.lang.reflect.Modifier
 
 @Composable
-fun TextFieldDefault(state: MutableState<TextFieldValue>) {
+fun TextFieldDefault(state: MutableState<TextFieldValue>, trail: ImageVector) {
     TextField(
         value = state.value,
         onValueChange = { state.value = it },
         singleLine = true,
         shape = RoundedCornerShape(13.dp),
+        trailingIcon = { Icon(trail, "", tint = Primary25) },
         colors = TextFieldDefaults.textFieldColors(backgroundColor = White98, cursorColor = Primary25, textColor = Primary25)
     )
 }
@@ -30,5 +35,5 @@ fun TextFieldDefault(state: MutableState<TextFieldValue>) {
 @Composable
 fun TextFieldDefaultPreview() {
     val testState = remember { mutableStateOf(TextFieldValue()) }
-    TextFieldDefault(testState)
+    TextFieldDefault(testState, Icons.Filled.Email)
 }
