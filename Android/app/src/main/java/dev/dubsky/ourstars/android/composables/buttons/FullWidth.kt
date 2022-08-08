c2a925b537ca48535f73cce05154fcb6384149b6
Clemens <dubcmsky@protonmail.com>
2022-08-08 15:40:27 +0200

    added composable structure

---
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/composables/buttons/FullWidth.kt
@@ -0,0 +1,15 @@
+package dev.dubsky.ourstars.android.composables.buttons
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FullWidth() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullWidthPreview() {
+
+}
