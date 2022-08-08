4ab44a32a8e734fd4fa3bfe93c8960edb83cfabe
Clemens <dubcmsky@protonmail.com>
2022-08-08 15:40:33 +0200

    added screen structure

---
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/HomeScreen.kt
@@ -0,0 +1,15 @@
+package dev.dubsky.ourstars.android.screens.open
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+
+}
---
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/secured/Settings.kt
@@ -0,0 +1,15 @@
+package dev.dubsky.ourstars.android.screens.secured
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingsScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+
+}
