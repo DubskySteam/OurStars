83879ba776dbbd0a69affdce3a3b83f998dbbaa6
Clemens <dubcmsky@protonmail.com>
2022-08-08 18:03:39 +0200

    added login screen

---
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/LoginScreen.kt
@@ -0,0 +1,47 @@
+package dev.dubsky.ourstars.android.screens.open
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import dev.dubsky.ourstars.android.composables.buttons.FullWidth
+
+@Composable
+fun LoginScreen() {
+
+    val emailState = remember { mutableStateOf(TextFieldValue())}
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+        ) {
+            Text("Email address")
+            TextField(emailState.value, { emailState.value = it })
+            Text("Username")
+            Text("Password")
+            Text("Repeat password")
+            FullWidth("Login", 14.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
