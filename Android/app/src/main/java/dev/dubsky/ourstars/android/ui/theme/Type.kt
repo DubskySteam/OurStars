174f174b4afe1fb82a4f3912b6194a720674062d
Clemens <dubcmsky@protonmail.com>
2022-08-09 14:15:03 +0200

    added Poppins as FontFamily

--- Android/app/src/main/java/dev/dubsky/ourstars/android/ui/theme/Type.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/ui/theme/Type.kt
@@ -2,14 +2,21 @@ package dev.dubsky.ourstars.android.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.dubsky.ourstars.android.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
