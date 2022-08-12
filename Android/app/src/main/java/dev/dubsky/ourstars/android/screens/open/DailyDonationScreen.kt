7911cbccbc3ad145f3f263734f677baeef442c4a
Clemens <dubcmsky@protonmail.com>
2022-08-12 13:13:11 +0200

    added daily donation screen

---
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/DailyDonationScreen.kt
@@ -0,0 +1,13 @@
+package dev.dubsky.ourstars.android.screens.open
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DailyDonationScreen() {}
+
+@Preview(showBackground = true)
+@Composable
+fun DailyDonationScreenPreview() {
+    DailyDonationScreen()
+}
