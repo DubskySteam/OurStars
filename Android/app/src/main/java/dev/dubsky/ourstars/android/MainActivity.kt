f2bdb1227b97672815f8fd20f7997cce99356cf0
Clemens <dubcmsky@protonmail.com>
2022-08-13 00:42:07 +0200

    removed default composables

--- Android/app/src/main/java/dev/dubsky/ourstars/android/MainActivity.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/MainActivity.kt
@@ -20,16 +20,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidTheme {
-        Greeting("Android")
-    }
-}
