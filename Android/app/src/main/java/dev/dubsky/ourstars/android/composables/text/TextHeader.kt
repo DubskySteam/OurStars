00112e3ddafe726fc66506925461940c71f65b2f
Clemens <dubcmsky@protonmail.com>
2022-08-12 15:00:01 +0200

    added header text

--- Android/app/src/main/java/dev/dubsky/ourstars/android/composables/text/TextHeader.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/composables/text/TextHeader.kt
@@ -1,2 +1,19 @@
 package dev.dubsky.ourstars.android.composables.text
 
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import dev.dubsky.ourstars.android.ui.theme.Poppins
+
+@Composable
+fun TextHeader(text: String, color: Color) {
+    Text(text, fontFamily = Poppins, color = color, fontSize = 24.sp)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextHeaderPreview() {
+    TextHeader("Donation", Color(0xFF31156B))
+}
