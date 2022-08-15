f827ef339b878a053a8137a7a36744c1e9b9aad7
Clemens <dubcmsky@protonmail.com>
2022-08-15 23:58:29 +0200

    put charity cards into a column

--- Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/CharitiesScreen.kt
+++ Android/app/src/main/java/dev/dubsky/ourstars/android/screens/open/CharitiesScreen.kt
@@ -1,5 +1,6 @@
 package dev.dubsky.ourstars.android.screens.open
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
@@ -7,11 +8,16 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import dev.dubsky.ourstars.android.composables.component.AvatarTextRow
 import dev.dubsky.ourstars.android.composables.component.CharityCard
+import dev.dubsky.ourstars.android.composables.text.TextCustom
+import dev.dubsky.ourstars.android.ui.theme.Primary25
 
 @Composable
 fun CharitiesScreen(
@@ -27,24 +33,28 @@ fun CharitiesScreen(
                 .fillMaxHeight()
                 ) {
             AvatarTextRow("Jane Doe", Icons.Filled.Person)
-            CharityCard(
-                "Child Protection",
-                "Improve rural schools in Genovia",
-                2300.0f,
-                1033.0f,
-            )
-            CharityCard(
-                "Child Protection",
-                "Improve rural schools in Genovia",
-                2300.0f,
-                1033.0f,
-            )
-            CharityCard(
-                "Child Protection",
-                "Improve rural schools in Genovia",
-                2300.0f,
-                1033.0f,
-            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                CharityCard(
+                    "Child Protection",
+                    "Improve rural schools in Genovia",
+                    2300.0f,
+                    1033.0f,
+                )
+                CharityCard(
+                    "Child Protection",
+                    "Improve rural schools in Genovia",
+                    2300.0f,
+                    1033.0f,
+                )
+                CharityCard(
+                    "Child Protection",
+                    "Improve rural schools in Genovia",
+                    2300.0f,
+                    1033.0f,
+                )
+            }
         }
     }
 }
